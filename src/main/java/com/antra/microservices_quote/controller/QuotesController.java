655faa280c3a9e14234c71f360e0440a459e47d9
BillChenJava <58630964+BillChenJava@users.noreply.github.com>
2020-08-05 09:49:32 -0400

    add print

--- src/main/java/com/antra/microservices_quote/controller/QuotesController.java
+++ src/main/java/com/antra/microservices_quote/controller/QuotesController.java
@@ -21,6 +21,7 @@ public class QuotesController {
 
     @RequestMapping(value="/get/{username}", method= RequestMethod.GET)
     public List<String> getQuotes(@PathVariable("username") final String username){
+        System.out.println("abc");
         return getQuotesByUsername(username);
     }
 
